5c227d89863d9421a4010efee1691531c7315eee
rajeswaricheepurupalli <115171022+rajeswaricheepurupalli@users.noreply.github.com>
2022-11-21 05:10:15 +0000

    Add files via upload

---
+++ delete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectingToSqlServer
+{
+    internal class delete
+    {
+        public static void Main()
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=rajeswari;Initial Catalog=Rajeswari;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine("Enter Employee id tat you want to delete:");
+            int EmployeeID = int.Parse(Console.ReadLine());
+            string deleteQuery = "delete from person where EmployeeID='" + EmployeeID + "'";
+            SqlCommand deleteCommand = new SqlCommand(deleteQuery, sqlConnection);
+            deleteCommand.ExecuteNonQuery();
+            Console.WriteLine("Data is deleted");
+            Console.ReadKey();
+
+            sqlConnection.Close();
+
+        }
+    }
+}
---
+++ update.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectingToSqlServer
+{
+    internal class update
+    {
+        public static void Main()
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=Rajeswari;Initial Catalog=rajeswari;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine("Enter Employee id");
+            int EmployeeID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the salary that you want to change");
+            int Salary = int.Parse(Console.ReadLine());
+            string updateQuery = "update person set Salary='" + Salary + "' where EmployeeID='" + EmployeeID + "'";
+            SqlCommand updateCommand = new SqlCommand(updateQuery, sqlConnection);
+            updateCommand.ExecuteNonQuery();
+            Console.WriteLine("Data updated");
+            sqlConnection.Close();
+            Console.ReadKey();
+        }
+    }
+}
